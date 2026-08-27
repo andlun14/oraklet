7eb5737d1474795997b96cf96583b2b754776a57
Andreas Lundsten <andlun14@edu.umea.se>
2026-08-27 11:35:09 +0200

    DIalog test

---
+++ dialog.py
@@ -0,0 +1,5 @@
+namn = input("Vad är ditt namn?: ")
+födelse_år = int(input("Vilket år var du född?: "))
+årtal = int(input("Vad är nuvarande året?: "))
+
+print(f"Hej, {namn}! Du är ungefär {årtal - födelse_år} år gammal.")
